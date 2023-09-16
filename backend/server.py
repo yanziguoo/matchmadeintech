9bfecdc0d0da6e998aa5d79fd583ff2b5b70c795
shane <shanec2k05@gmail.com>
2023-09-16 15:30:36 -0400

    finish get_user endpoint

--- backend/server.py
+++ backend/server.py
@@ -8,42 +8,8 @@ dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
 PA_TOKEN = os.environ.get("PA_TOKEN")
 
-gql_query = """
-{
-  user1: user(login: $username) { # my username
-    login
-    id
-    contributionsCollection {
-        contributionCalendar {
-            totalContributions
-        }
-    }
-    pinnedItems(first: 6, types: REPOSITORY) {
-      nodes {
-          ... on Repository {
-              name
-              id
-              languages(first: 10) {
-                  edges {
-                      size
-                  }
-                  nodes {
-                      name
-                  }
-              }
-          }
-      }
-    }
-  }
-}
-"""
-
 headers = {
     "Authorization": f"Bearer {PA_TOKEN}",
-}
-
-data = {
-    'query': gql_query,
 }
 
 app = Flask(__name__)
@@ -55,9 +21,50 @@ def hello_world():
 
 @app.route('/get_user/<username>')
 def get_user(username):
-  response = requests.post("https://api.github.com/graphql", headers=headers, json=data)
-  # Check if the request was successful (status code 200)
-  if response.status_code == 200:
-      result = response.json()
-      print(result)
-  return response.json()
+    gql_query = """
+query GetUser($username: String!) {
+    user1: user(login: $username) { # my username
+        login
+        id
+        contributionsCollection {
+            contributionCalendar {
+                totalContributions
+            }
+        }
+        pinnedItems(first: 6, types: REPOSITORY) {
+            nodes {
+                ... on Repository {
+                    name
+                    id
+                    languages(first: 10) {
+                        edges {
+                            size
+                        }
+                        nodes {
+                            name
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
+    data = {
+        'query': gql_query,
+        'variables': {
+           "username": username
+        }
+    }
+
+    response = requests.post("https://api.github.com/graphql", headers=headers, json=data)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        result = response.json()
+        print(f"-----------------\n{result}")
+    else:
+        print(f"Request failed with status code {response.status_code}")
+        print(response.text)
+        raise ValueError
+    return response.json()
