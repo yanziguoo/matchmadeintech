7f3355953765de8129c95e2a90e657f63692bc28
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-09-16 14:11:54 -0400

    get api for one user

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,25 @@
 from flask import Flask
+import requests
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+PA_TOKEN = os.environ.get("PA_TOKEN")
+
+headers = {
+    "Authorization": f"Bearer {PA_TOKEN}",
+}
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello world!'
+
+
+@app.route('/get_user/<username>')
+def get_user(username):
+  response = requests.get("https://api.github.com/users/aazzazron", headers=headers)
+  return username
