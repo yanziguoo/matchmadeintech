0b0e90021f554b57a964b1ef759be1984843f4fd
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-09-16 17:43:13 -0400

    add means

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask_cors import CORS
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -48,6 +49,11 @@ query GetUser($username: String!) {
 }
 """
 
+with open("./data/kmeansmodel.pkl", "rb") as f:
+    model = pickle.load(f)
+with open("./data/means.csv", "rb") as f:
+    means = [float(x) for x in f.readlines()]
+
 
 @app.route('/')
 def hello_world():
@@ -74,17 +80,16 @@ def get_user(username):
     return response.json()
 
 
-def get_model():
-    with open("./data/kmeansmodel.pkl", "rb") as f:
-        model = pickle.load(f)
-    return model
+def standardize(data):
+    print(means)
 
 
 @app.route('/find_matches/<username>')
 def find_matches(username):
-    data = get_user(username)
-    model = get_model()
-    
+    global model, means
+
+    data = standardize(get_user(username))
     print(model.cluster_centers_)
 
     return data
+
