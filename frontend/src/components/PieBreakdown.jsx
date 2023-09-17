f88f265a24fe952816b5943161adbcedb5fd9b54
Tanay Kumar <tanaykumar29@gmail.com>
2023-09-17 00:14:15 -0400

    graph
    2qewdaed

--- frontend/src/components/PieBreakdown.jsx
+++ frontend/src/components/PieBreakdown.jsx
@@ -1,39 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 
 export default function PieBreakdown() {
-    const [pieData, setPieData] = useState([]); // [ { title: 'One', value: 10, color: '#E38627' }, ...
-    // const langs = props.langs;
-    
-    // for username, row in picked.iterrows():
-    //     curr = {}
-    //     curr['username'] = username
-    //     curr['id'] = row['Id']
-    //     curr['contributions'] = row['Contributions']
-    //     curr['languages'] = {}
-    //     for lang in col[2:]:
-    //         if row[lang] > 0:
-    //             curr['languages'][lang] = row[lang]
-    //     ret.append(curr)
+  const [pieData, setPieData] = useState([]);
+  
+  const sampleData = [
+    { label: 'C++', quantity: 3 },
+    { label: 'Python', quantity: 1 },
+    { label: 'Java', quantity: 2 },
+    { label: 'Brainfuck', quantity: 3 },
+    { label: 'Other', quantity: 2 },
+  ];
 
-    const languages = {
-        python: 0.50,
-        javascript: 0.50,
+  const colors = ['#E38627', '#C13C37', '#6A2135', '#339A65', '#A63EA9'];
+  const total = sampleData.reduce((acc, curr) => acc + curr.quantity, 0);
+  
+  useEffect(() => {
+    const data = [];
+    for (const [index, item] of sampleData.entries()) {
+      const percentage = (item.quantity / total) * 100;
+      data.push({ title: item.label, value: percentage, color: colors[index] });
     }
+    setPieData(data);
+  }, []);
 
-    useEffect(() => {
-        const data = [];
-        for (const lang in languages) {
-            data.push({ title: lang, value: languages[lang], color: '#E38627' });
-        }
-        setPieData(data);
-    }, []);
-
-    return (
-        <PieChart
-            data={pieData}
-            style={{ backgroundColor: 'transparent' }}
-        />
-
-    )
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <PieChart
+        data={pieData}
+        style={{ height: '150px', width: '150px' }}
+      />
+      <div style={{ marginTop: '10px' }}>
+        {pieData.map((dataEntry, index) => (
+          <div key={index} style={{ color: dataEntry.color, marginBottom: '5px' }}>
+            {`${dataEntry.title}: ${Math.round(dataEntry.value / total * 100)}%`}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
+import React from 'react';
 import PieBreakdown from './PieBreakdown';
-import '../styles/UserCard.css'
+import '../styles/UserCard.css';
 
 export default function UserCard(props) {
-    const pfp = props.pfp;
-    const name = props.name;
-    const join = props.join;
-    const lang = props.lang;
-    const commit = props.commit;
+  const pfp = props.pfp;
+  const name = props.name;
+  const commit = props.commit;
 
-
-    return (
-      <div className="positioning-container">
-        <div className="backdrop"></div>
-        <div className="card-container">
-           <img src={pfp} className="img" alt="profile" />
-          <div className="name">{name}</div>
-          <div className="text">Joined: {join}</div>
-          <div className="text">Language breakdown: {lang}</div>
-          <div className="text">Commits: {commit}</div> 
+  return (
+    <div className="positioning-container">
+      <div className="backdrop"></div>
+      <div className="card-container">
+        <img src={pfp} className="img" alt="profile" />
+        <div className="name">{name}</div>
+        <div className="text">Commits: {commit}</div>
+        <div className="pie-chart-container">
           <PieBreakdown />
         </div>
       </div>
-      )
-    }
+    </div>
+  );
+}
