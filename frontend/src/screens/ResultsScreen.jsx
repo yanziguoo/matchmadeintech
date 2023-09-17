626f91bcaa419a4031781e5a0d2535099a355905
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-09-16 22:39:45 -0400

    connect front and back

--- frontend/src/screens/ResultsScreen.jsx
+++ frontend/src/screens/ResultsScreen.jsx
@@ -4,18 +4,28 @@ import '../styles/ResultsScreen.css';
 import UserCard from '../components/UserCard';
 import tanay from '../assets/tanay.jpeg';
 import bannerLogo from '../assets/banner logo.png';
+import axios from 'axios';
 
 export default function ResultsScreen() {
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState('');
+  const [matches, setMatches] = useState([]);
   const location = useLocation();
 
   useEffect(() => {
     const username = location.state;
     console.log(username);
     setUsername(username);
-    setLoading(false);
+    axios.get(`http://localhost:5000/find_matches/${username}`)
+      .then(res => {
+        console.log(res.data);
+        setMatches(res.data);
+        setLoading(false);
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
   }, []);
+
 
   if (loading) {
     return (
